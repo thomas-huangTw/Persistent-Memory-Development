aa795f8531c802e4c8dc1811490d0d44d528014d
thomashuang <guitar10833@gmail.com>
2020-05-11 10:14:15 +0800

    Update ManangerModethree.py

--- ManangerModethree.py
+++ ManangerModethree.py
@@ -26,58 +26,128 @@ while(1):
         #The pmem blocks size you want to create
         create_size = input("create size:")
         
-        worker1 = requests.post(Worker1_IP + '/create', json={"folder_name":folder_name,"create_size":create_size})
-        if worker1.ok:
-            if str(worker1.text)=="Worker1: Your required capacity is too large!(Or the format is incorrect)":
-                worker3 = requests.post(Worker3_IP + '/create', json={"folder_name":folder_name,"create_size":create_size})
-                if worker3.ok:
-                    print(worker3.text+"\n")
-            else:
-                print(worker1.text+"\n")
+        #try to post json to Worker1 /create api
+        try:
+            worker = requests.post(Worker1_IP + '/create', json={"folder_name":folder_name,"create_size":create_size})
+            if worker.ok:
+                if str(worker.text)=="Worker1: Your required capacity is too large!(Or the format is incorrect)":
+                    pass
+                else:
+                    print(worker.text+"\n")
+                    continue
+        except requests.ConnectionError:
+            pass
+
+        #try to post json to Worker2 /create api
+        try:
+            worker = requests.post(Worker2_IP + '/create', json={"folder_name":folder_name,"create_size":create_size})
+            if worker.ok:
+                if str(worker.text)=="Worker2: Your required capacity is too large!(Or the format is incorrect)":
+                    pass
+                else:
+                    print(worker.text+"\n")
+                    continue
+        except requests.ConnectionError:
+            pass
+
+        #try to post json to Worker3 /create api
+        try:
+            worker = requests.post(Worker3_IP + '/create', json={"folder_name":folder_name,"create_size":create_size})
+            if worker.ok:
+                if str(worker.text)=="Worker3: Your required capacity is too large!(Or the format is incorrect)":
+                    pass
+                else:
+                    print(worker.text+"\n")
+                    continue
+        except requests.ConnectionError:
+            pass
+
+        print("Your required capacity is too large!(Or the format is incorrect)\n")
 
     if str(mode_chose)==str("1"):
         #The peme blocks uuid you want to delete
         Delete_SpaceName = input("The name of the region you need to delete:")
-        Worker1_delete = requests.post(Worker1_IP + '/DeleteUUID', json={"UUID":Delete_SpaceName})
-        CallDeleteFun = Worker1_delete.text
-        if str(CallDeleteFun)=="Worker1. Delete Success.":
-            print("Work1. Delete Success.\n")
-        
-        #if can't find on worker1., then find other workers
-        elif str(CallDeleteFun)=="Worker1. No this UUID." and (requests.post(Worker3_IP + '/FindUUID', json={"UUID":Delete_SpaceName})).text!="Worker3. Find Fail.\n":
-            DeleteWorker2UUID = requests.post(Worker3_IP + '/DeleteUUID', json={"UUID":Delete_SpaceName})
-            print(DeleteWorker2UUID.text+"\n")
-        
-        #if can't find on all workers
-        else:
-            print("Workers UUID don't exist.\n")
+
+        #try to post json to Worker1 /DeleteUUID api
+        try:
+            Worker1_delete = requests.post(Worker1_IP + '/DeleteUUID', json={"UUID":Delete_SpaceName})
+            CallDeleteFun = Worker1_delete.text
+            if str(CallDeleteFun)=="Worker1. Delete Success.":
+                print("Work1. Delete Success.\n")
+                continue
+        except requests.ConnectionError:
+            pass
+
+        #try to post json to Worker2 /DeleteUUID api
+        try:
+            Worker2_delete = requests.post(Worker2_IP + '/DeleteUUID', json={"UUID":Delete_SpaceName})
+            CallDeleteFun = Worker2_delete.text
+            if str(CallDeleteFun)=="Worker2. Delete Success.":
+                print("Work2. Delete Success.\n")
+                continue
+        except requests.ConnectionError:
+            pass
+
+        #try to post json to Worker3 /DeleteUUID api
+        try:
+            Worker3_delete = requests.post(Worker3_IP + '/DeleteUUID', json={"UUID":Delete_SpaceName})
+            CallDeleteFun = Worker3_delete.text
+            if str(CallDeleteFun)=="Worker3. Delete Success.":
+                print("Work3. Delete Success.\n")
+                continue
+        except requests.ConnectionError:
+            pass
+
+        print("Workers UUID don't exist.\n")
 
     if str(mode_chose)==str("2"):
         ModeTwoChose = input("Please enter your required items: 1.pmem list availcapacity.; 2.pmem list blkdev.\nItem selection:")
         if ModeTwoChose=="1":
             WorkerOneAvailcapacity = requests.post(Worker1_IP + '/availcapacity')
-            WorkerTwoAvailcapacity = requests.post(Worker3_IP + '/availcapacity')
+            WorkerTwoAvailcapacity = requests.post(Worker2_IP + '/availcapacity')
+            WorkerThreeAvailcapacity = requests.post(Worker3_IP + '/availcapacity')
+            
             if WorkerOneAvailcapacity=="":
                 print("None.")
             else:
                 print("Worker1:\n"+WorkerOneAvailcapacity.text)
             print("\n")
+            
             if WorkerTwoAvailcapacity=="":
                 print("None.")
             else:
-                print("Worker3:\n"+WorkerTwoAvailcapacity.text)
+                print("Worker2:\n"+WorkerTwoAvailcapacity.text)
+            print("\n")
+            
+            if WorkerThreeAvailcapacity=="":
+                print("None.")
+            else:
+                print("Worker3:\n"+WorkerThreeAvailcapacity.text)
+            print("\n")
+            
         elif ModeTwoChose=="2":
             WorkerOneBlkdev = requests.post(Worker1_IP + '/blkdev')
-            WorkerTwoBlkdev = requests.post(Worker3_IP + '/blkdev')
+            WorkerTwoBlkdev = requests.post(Worker2_IP + '/blkdev')
+            WorkerThreeBlkdev = requests.post(Worker3_IP + '/blkdev')
+            
             if WorkerOneBlkdev.text=="":
                 print("Worker1: None.\n")
             else:
                 print("Worker1:\n"+WorkerOneBlkdev.text+"\n")
             print("\n")
+            
             if WorkerTwoBlkdev.text=="":
+                print("Worker2 None.\n")
+            else:
+                print("Worker2:\n"+WorkerTwoBlkdev.text+"\n")
+            print("\n")
+            
+            if WorkerThreeBlkdev.text=="":
                 print("Worker3 None.\n")
             else:
-                print("Worker3:\n"+WorkerTwoBlkdev.text+"\n")
+                print("Worker3:\n"+WorkerThreeBlkdev.text+"\n")
+            print("\n")
+            
         else:
             print("wrong format.\n")
 
